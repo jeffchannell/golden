b8a785a0c91b68d9186fc257d0683b7b797a2c4c
Jeff Channell <me@jeffchannell.com>
2018-10-19 09:25:57 -0400

    fixes

--- ratio.go
+++ ratio.go
@@ -3,20 +3,26 @@
 
 package golden
 
-import "math"
+import (
+	"math"
+)
 
-// Ratio finds the golden ratio given two numbers a and b and returns c
-// when a < b, a < c < b
-// when a > b, c > a > b
-// when a == b, a == b == c
+// Ratio finds the golden ratio between two numbers a and b
 func Ratio(a float64, b float64) (c float64) {
 	// use interior division to find c
 	// 1. Having a line segment AB, construct a perpendicular BC at point B, with BC half the length of AB. Draw the hypotenuse AC.
-	ab := math.Abs(b - a)          // get the distance between the two "points" (a,0) and (b,0)
+	// get the distance between the two "points" (a,0) and (b,0)
+	pos := (a > b)
+	ab := math.Abs(a - b)
 	bc := ab / 2                   // get the distance of the perpendicular (b,0) and (b,c)
 	ac := math.Sqrt(ab*ab + bc*bc) // get the distance of the hypotenuse
 	// 2. Draw an arc with center C and radius BC. This arc intersects the hypotenuse AC at point D.
 	// 3. Draw an arc with center A and radius AD. This arc intersects the original line segment AB at point S. Point S divides the original line segment AB into line segments AS and SB with lengths in the golden ratio.
-	c = a + (ac - bc) // instead of an arc, we know that BC is the radius so that can be subtracted from AC to get the distance of AD and to derive AS (it's the same distance as AD), adjusted for a
+	// instead of an arc, we know that BC is the radius so that can be subtracted from AC to get the distance of AD and to derive AS (it's the same distance as AD), adjusted for a
+	if pos {
+		c = b - (bc - ac)
+	} else {
+		c = b + (bc - ac)
+	}
 	return
 }
--- ratio_test.go
+++ ratio_test.go
@@ -13,22 +13,29 @@ func TestRatio(t *testing.T) {
 		e float64
 	}{
 		// tests where a < b
-		{a: 0, b: 8, e: 5},
-		{a: 0, b: 13, e: 8},
-		{a: 3, b: 16, e: 11},
-		{a: -3, b: 10, e: 5},
+		{a: 0, b: 5, e: 2},
+		{a: 0, b: 8, e: 3},
+		{a: 0, b: 13, e: 5},
+		{a: 3, b: 16, e: 8},
+		{a: -3, b: 10, e: 2},
 		// tests where a > b
-		{a: 5, b: 0, e: 8},
-		{a: 8, b: 0, e: 13},
+		{a: 5, b: 0, e: 3},
+		{a: 8, b: 0, e: 5},
+		{a: 13, b: 0, e: 8},
+		{a: 16, b: 3, e: 11},
+		{a: 10, b: -3, e: 5},
 		// tests where a == b
 		{a: 0, b: 0, e: 0},
 		{a: 1, b: 1, e: 1},
 		{a: -1, b: -1, e: -1},
 	}
+	// loop through the tests
 	for i, test := range tests {
-		f := Ratio(test.a, test.b)
-		if r := int(math.Round(f)); int(test.e) != r {
-			t.Fatalf("case %d: Expected %v but got %d; %v", i, test.e, r, f)
+		// get the ratio
+		c := Ratio(test.a, test.b)
+		// check the expected result against the actual one
+		if r := int(math.Round(c)); int(test.e) != r {
+			t.Fatalf("case %d: Expected %v but got %d; %v", i, test.e, r, c)
 		}
 	}
 }
